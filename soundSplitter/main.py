dc84af7dc5216e5a8ebef6660bbc99113ede74d2
apzzd <59454160+apzzd@users.noreply.github.com>
2023-04-06 11:57:00 +0200

    make it perfect + slice 'em up

--- soundSplitter/main.py
+++ soundSplitter/main.py
@@ -1,17 +1,17 @@
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 
-print(AudioSegment)
 
-myaudio = AudioSegment.from_file("myAudio.wav", "wav")
-chunk_length_ms = 4000
-chunks = make_chunks(myaudio, chunk_length_ms)
+def sliceWav(segLength, fileName):
+	myaudio = AudioSegment.from_file(fileName + ".wav", "wav")
+	chunk_length_ms = segLength * 1000
+	chunks = make_chunks(myaudio, chunk_length_ms)
 
-for i, chunk in enumerate(chunks):
-    chunk_name = "chunk{0}.wav".format(i)
-    print("exporting", chunk_name)
-    chunk.export(chunk_name, format="wav")
+	for i, chunk in enumerate(chunks):
+    		chunk_name = "slicedAudio/" + fileName + "Chunk{0}.wav".format(i)
+    		print("exporting", chunk_name)
+    		chunk.export(chunk_name, format="wav")
 
-# TODO:
-#  - put all spliced files in a directory to get rid of the mess
-#  - loop through the training data
+
+for fname in ["Morning1", "Morning2", "Afternoon2", "Night1", "Night2"]:
+	sliceWav(4, fname)
