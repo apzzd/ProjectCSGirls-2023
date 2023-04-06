aee952bd475a64ed2ca7b2f2d00276551a9cb972
apzzd <59454160+apzzd@users.noreply.github.com>
2023-04-06 16:23:47 +0200

    easy identification

--- soundSplitter/main.py
+++ soundSplitter/main.py
@@ -8,7 +8,7 @@ def sliceWav(segLength, fileName):
 	chunks = make_chunks(myaudio, chunk_length_ms)
 
 	for i, chunk in enumerate(chunks):
-    		chunk_name = "slicedAudio/" + fileName + "Chunk{0}.wav".format(i)
+    		chunk_name = "slicedAudio/" + fileName + "_{0}-{1}.wav".format(i * segLength, (i + 1) * segLength)
     		print("exporting", chunk_name)
     		chunk.export(chunk_name, format="wav")
 
