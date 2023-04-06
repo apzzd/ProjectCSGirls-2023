429ab0e9d24841ddad78da72eb586db766377efb
apzzd <59454160+apzzd@users.noreply.github.com>
2023-04-06 10:05:36 +0200

    fix sound splitting py file

--- soundSplitter/main.py
+++ soundSplitter/main.py
@@ -11,3 +11,7 @@ for i, chunk in enumerate(chunks):
     chunk_name = "chunk{0}.wav".format(i)
     print("exporting", chunk_name)
     chunk.export(chunk_name, format="wav")
+
+# TODO:
+#  - put all spliced files in a directory to get rid of the mess
+#  - loop through the training data
